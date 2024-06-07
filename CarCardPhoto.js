d3afd80f8759fcae4f1ec0f0aac4ad2324942f32
Kostiantyn Polishko <167745085+KostiantynPolishko2@users.noreply.github.com>
2024-06-07 08:20:15 +0300

    release v1.0.1 (#3)
    * release v1.0.0.0 (#1)
    
    * 1 Init main files html, css, js
    
    * 2 Created template of main car-card
    
    * 3 Created style_car-card.css
    
    * 4 Done class CarCardPhoto
    
    class CarCardPhoto has method createCarCard that dynamically create container with nested tags.
    
    * 5 Done method setTxtCarCard() in class CarCardPhoto
    
    * 6 Create function getApiObjByNumber and getCarPhoto
    
    getApiObjByNumber retrieve general object;
    getCarPhoto retrieve requested fields of class CarCardPhoto from general object getApiObjByNumber.
    
    * 7 Substitude background-image of <div> on <img>
    
    Managed main structure of loading carCardPhoto with related to it information.
    
    * 8 Added form for search car dates by number or vin code.
    
    * 9 Connected input form with function createCaPhoto()
    
    * release v1.0.1 (#2)
    
    * release v1.0.0.0 (#1)
    
    * 1 Init main files html, css, js
    
    * 2 Created template of main car-card
    
    * 3 Created style_car-card.css
    
    * 4 Done class CarCardPhoto
    
    class CarCardPhoto has method createCarCard that dynamically create container with nested tags.
    
    * 5 Done method setTxtCarCard() in class CarCardPhoto
    
    * 6 Create function getApiObjByNumber and getCarPhoto
    
    getApiObjByNumber retrieve general object;
    getCarPhoto retrieve requested fields of class CarCardPhoto from general object getApiObjByNumber.
    
    * 7 Substitude background-image of <div> on <img>
    
    Managed main structure of loading carCardPhoto with related to it information.
    
    * 8 Added form for search car dates by number or vin code.
    
    * 9 Connected input form with function createCaPhoto()
    
    * 1 Managed correcteness for inputed number via regular expression
    
    function isNumber(number) with regex.
    
    * 2 Done treatment of error for instance of class CarCardPhoto
    
    Treatment of error 404.
    Treatment of uncorrect format inputted for car number.

---
+++ CarCardPhoto.js
@@ -0,0 +1,68 @@
+class CarCardPhoto{
+    divContainer;
+    carPhotoData;
+
+    constructor(id){
+        this.identifier = id;
+        this.createCarCard();
+        this.setDefault();
+    }
+
+    createCarCard(){
+        this.divContainer = $('<div>').attr('id', this.identifier).addClass('car-card__img')[0];
+        $(this.divContainer).append($("<img src='./img/car_icon.jpg' alt='car'>").addClass('img__car-card__main'));
+
+        $(this.divContainer).append("<div class='car-card__content car-card__overlay'></div>");
+        $(this.divContainer).find('div.car-card__content').append("<div class='car-card__content-top'></div>");
+        $(this.divContainer).find('div.car-card__content').append("<div class='car-card__content-bottom'></div>");
+
+        $(this.divContainer).find('div.car-card__content-top').append("<div class='plate-number'></div>");
+        $(this.divContainer).find('div.car-card__content-top').append("<div><span></span></div>")
+        $(this.divContainer).find('div.car-card__content-top').append("<div class='plate-data__registr'><span></span></div>");
+        $(this.divContainer).find('div.plate-number').append("<div class='plate-number__flag'></div>");
+        $(this.divContainer).find('div.plate-number').append("<div class='plate-number__text'><span></span></div>");
+
+        $(this.divContainer).find('div.plate-number__flag').append("<div class='plate-number__flag-icon'></div>");
+        let flag = $(this.divContainer).find('div.plate-number__flag-icon')[0];
+        $(flag).append("<div class='flag-icon__blue'></div>").append("<div class='flag-icon__yellow'></div>");
+        $(this.divContainer).find('div.plate-number__flag').append("<div class='plate-number__code-country'><span></span></div>");
+        $(this.divContainer).find('div.car-card__content-bottom').append("<div class='car-card__data-model'><span></span></div>");
+        $(this.divContainer).find('div.car-card__content-bottom').append("<div class='car-card__mark-model'><span></span></div>");
+    }
+   
+    setCarPhotoTxt(carPhotoData){
+        $(this.divContainer).find('img').attr('src', carPhotoData.photoUrl);
+
+        $(this.divContainer).find('div.plate-number__code-country > span').text(carPhotoData.codeCountry);
+        $(this.divContainer).find('div.plate-number__text > span').text(carPhotoData.numberText);
+        $(this.divContainer).find('div.plate-data__registr > span').text(carPhotoData.dataRegistr);
+        $(this.divContainer).find('div.car-card__data-model > span').text(carPhotoData.dataModel);
+        $(this.divContainer).find('div.car-card__mark-model > span').text(carPhotoData.markModel);
+
+        $(this.divContainer).find('div.content-top__message-info > span').empty();
+        $(this.divContainer).find('div.plate-number').next().removeClass();
+    }
+
+    setDefault(number='XXDDDDXX'){
+        $(this.divContainer).find('img').attr('src', './img/car_icon.jpg');
+
+        $(this.divContainer).find('div.plate-number__code-country > span').empty();
+        $(this.divContainer).find('div.plate-number__text > span').text(number);
+        $(this.divContainer).find('div.plate-data__registr > span').empty();
+        $(this.divContainer).find('div.car-card__data-model > span').empty();
+        $(this.divContainer).find('div.car-card__mark-model > span').empty();
+    }
+
+    setInputUnFormat(number){
+        this.setDefault(number);
+        $(this.divContainer).find('div.plate-number').next().addClass('content-top__message-info');
+        $(this.divContainer).find('div.content-top__message-info > span').text('UNCORRECT FORMAT!');
+    }
+
+    setEror404(number){
+        this.setDefault(number);
+        $(this.divContainer).find('div.plate-number').next().addClass('content-top__message-info');
+        $(this.divContainer).find('div.content-top__message-info > span').text('NUMBER IS ABSENT!');
+        $(this.divContainer).find('img.img__car-card__main').attr('src', './img/error404.jpg');
+    }
+}
