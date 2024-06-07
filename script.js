15f63cb723816d18fa909150314527241d487434
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-07 21:13:39 +0300

    release v1.0.1 (#2)
    * release v1.0.0.0 (#1)
    
    * 1 Init main files html, css, js
    
    * 2 Created template of main car-card
    
    * 3 Created style_car-card.css
    
    * 4 Done class CarCardPhoto
    
    class CarCardPhoto has method createCarCard that dynamically create container with nested tags.
    
    * 5 Done method setTxtCarCard() in class CarCardPhoto
    
    * 6 Create function getApiObjByNumber and getCarPhoto
    
    getApiObjByNumber retrieve general object;
    getCarPhoto retrieve requested fields of class CarCardPhoto from general object getApiObjByNumber.
    
    * 7 Substitude background-image of <div> on <img>
    
    Managed main structure of loading carCardPhoto with related to it information.
    
    * 8 Added form for search car dates by number or vin code.
    
    * 9 Connected input form with function createCaPhoto()
    
    * 1 Managed correcteness for inputed number via regular expression
    
    function isNumber(number) with regex.
    
    * 2 Done treatment of error for instance of class CarCardPhoto
    
    Treatment of error 404.
    Treatment of uncorrect format inputted for car number.

---
+++ script.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', async (e) => {
+    console.log('Start');
+
+    const carCardPhoto = new CarCardPhoto('car_photo_main');
+    document.querySelector('body').appendChild(carCardPhoto.divContainer);
+
+    document.search.send.addEventListener('click', (e)=>{       
+        let number = e.target.previousElementSibling.value.replace(/\s/g, '');
+        number = number.replace(/[a-z]/gi, x => x.toUpperCase());
+
+        if(isNumber(number)){
+            createCarPhoto(number, carCardPhoto);
+            e.target.previousElementSibling.value = number;
+        }
+        else{
+            carCardPhoto.setInputUnFormat(number);
+            document.search.reset();         
+        }       
+    })
+})
+
+const isNumber = (number) => {
+    const regex = /\b[A-Z]{2}\d{4}[A-Z]{2}\b/i;
+    return regex.test(number);
+}
+
+const createCarPhoto = async(nomer, carCardPhoto) => {
+    let url = 'https://baza-gai.com.ua/nomer/';
+    const key = '53f98d3aa5e27428971d52008bedee4a';
+
+    try{
+        carCardPhoto.setCarPhotoTxt(getCarPhotoData(await getApiObjByNumber(url, nomer, key)));
+    }
+    catch(error){
+        carCardPhoto.setEror404(nomer);
+    }
+}
+
+const getApiObjByNumber = async (url, nomer, key) => {
+
+    const responce = await fetch(`${url}${nomer}`, {headers: {'Accept': 'application/json', 'X-Api-Key': key}});
+    if(responce.status === 200){
+        return responce.json();
+    }
+    else{
+        throw new Error();
+    }
+}
+
+const getCarPhotoData = (apiObj) => {
+
+    let carPhotoData = {};
+    carPhotoData.photoUrl = apiObj.photo_url??= './img/error404.jpg' ;
+    carPhotoData.codeCountry = 'UA';
+    carPhotoData.numberText = apiObj.digits??= 'XX 0000 YY';
+    carPhotoData.dataRegistr = apiObj.operations[0].registered_at??= 'dd.mm.yyyy';
+    carPhotoData.dataModel = apiObj.model_year??= 'yyyy';
+    carPhotoData.markModel = `${apiObj.vendor??= 'x-vendor'} ${apiObj.model??= 'y-model'}`
+    
+    return carPhotoData;
+}
