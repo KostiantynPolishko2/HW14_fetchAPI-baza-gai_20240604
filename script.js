b0180887054d39344ebd86352c214885d95d4cf1
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-07 14:03:28 +0300

    2 Done functions getApiObjByVendorModel(), getUrlPhotoCar(), getCarPhotoDataToNext()
    All these three function prepare object with dates for create instance of class CarPhotoNext().

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+const key = '53f98d3aa5e27428971d52008bedee4a';
+
 document.addEventListener('DOMContentLoaded', async (e) => {
     console.log('Start');
 
     // const carPhotoNext = new CarPhotoNext('car-photo__next');
     // document.querySelector('body').appendChild(carPhotoNext.divContainer);
+
+    // getUrlPhotoCar(await getApiObjByVendorModel('bmw', '328i'), 2014);
 
     const carPhotoMain = new CarPhotoMain('car-photo__main');
     document.querySelector('body').appendChild(carPhotoMain.divContainer);
@@ -28,20 +32,27 @@ const isNumber = (number) => {
 }
 
 const createCarPhoto = async(nomer, carCardPhoto) => {
-    let url = 'https://baza-gai.com.ua/nomer/';
-    const key = '53f98d3aa5e27428971d52008bedee4a';
-
     try{
-        carCardPhoto.setCarPhotoTxt(getCarPhotoData(await getApiObjByNumber(url, nomer, key)));
+        const result = await getApiObjByNumber(nomer);
+        carCardPhoto.setCarPhotoTxt(getCarPhotoData(result));
+
+        //===create list photos of previous cars===
+        const cars = Array.from(result.operations);
+        if(cars.length > 1){
+            for(let i = 1; i != cars.length; i++){
+                let url_photo = getUrlPhotoCar(await getApiObjByVendorModel(cars[i].vendor, cars[i].model));
+                console.log(i, getCarPhotoDataToNext(cars[i], url_photo, nomer));
+            }
+        }
     }
     catch(error){
         carCardPhoto.setEror404(nomer);
     }
 }
 
-const getApiObjByNumber = async (url, nomer, key) => {
+const getApiObjByNumber = async (nomer) => {
 
-    const responce = await fetch(`${url}${nomer}`, {headers: {'Accept': 'application/json', 'X-Api-Key': key}});
+    const responce = await fetch(`https://baza-gai.com.ua/nomer/${nomer}`, {headers: {'Accept': 'application/json', 'X-Api-Key': key}});
     if(responce.status === 200){
         return responce.json();
     }
@@ -61,4 +72,31 @@ const getCarPhotoData = (apiObj) => {
     carPhotoData.markModel = `${apiObj.vendor??= 'x-vendor'} ${apiObj.model??= 'y-model'}`
     
     return carPhotoData;
+}
+
+const getCarPhotoDataToNext = (apiObj, photo_url, number) => {
+    let carPhotoDataNext = {};
+    carPhotoDataNext.photoUrl = photo_url??= './img/error404.jpg' ;
+    carPhotoDataNext.codeCountry = 'UA';
+    carPhotoDataNext.numberText = number??= 'XX 0000 YY';
+    carPhotoDataNext.dataRegistr = apiObj.registered_at??= 'dd.mm.yyyy';
+    carPhotoDataNext.dataModel = apiObj.model_year??= 'yyyy';
+    carPhotoDataNext.markModel = `${apiObj.vendor??= 'x-vendor'} ${apiObj.model??= 'y-model'}`
+    
+    return carPhotoDataNext;
+}
+
+const getApiObjByVendorModel = async (vendor, model) => {
+
+    const responce = await fetch(`https://baza-gai.com.ua/make/${vendor.toLowerCase()}/${model.toLowerCase()}`, {headers: {'Accept': 'application/json', 'X-Api-Key': key}});
+    if(responce.status === 200){
+        return responce.json();
+    }
+    else{
+        return null;
+    }
+}
+
+const getUrlPhotoCar = (apiObj) => {
+    return apiObj.catalog_model.photo_url??= './img/car_come_soon.jpg';
 }
